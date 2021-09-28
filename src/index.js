b2ac187df8d96d1eaca029100424d627c6f7b8da
Ritik Raj <ritik2903raj@gmail.com>
2021-09-28 18:54:55 +0530

    time date

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@
 //modern javascript me import likh ke ho jata hai
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+var t="Ritik";
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
 ReactDOM.render(
 <>
-<h1>Web Series!</h1>
-<p>My Favourite</p>
-<ul>
-  <li>Money Heist</li>
-  <li>Lucifer</li>
-  <li>Dark</li>
-  <li>Panchayat</li>
-  <li>Aspirants</li>
-</ul>
-</>
-  ,
+<h1>{`Hello, My Name is ${t}`}</h1>
+<p>{`today Date is = ${currDate}`}</p>
+<p>{`today time is = ${currTime}`}</p>
+
+</>,
   document.getElementById("root"));
