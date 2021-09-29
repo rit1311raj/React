185e47c48cadfbee4516e64ba66ff497088561b7
Ritik Raj <ritik2903raj@gmail.com>
2021-09-29 13:16:32 +0530

    fresh-up

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 var t="Ritik Raj";
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+//const currDate = new Date().toLocaleDateString();
+//const currTime = new Date().toLocaleTimeString();
 const img1= "https://picsum.photos/200/300";
 const img2= "https://picsum.photos/250/300";
 const img3= "https://picsum.photos/300/300";
 const links= "https://www.facebook.com";
+//<h1>{`Hello, My Name is ${t}`}</h1>
+//<p>{`today Date is = ${currDate}`}</p>
+//<p>{`today time is = ${currTime}`}</p>
 
 ReactDOM.render(
 <>
-<h1>{`Hello, My Name is ${t}`}</h1>
-<p>{`today Date is = ${currDate}`}</p>
-<p>{`today time is = ${currTime}`}</p>
 <h2 className="heading">My name is {t}</h2>
 
 <div className="div_img">
