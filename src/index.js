472be02b276e07dd36b0a0dff05a4ad3667f598b
Ritik Raj <ritik2903raj@gmail.com>
2021-09-29 11:45:42 +0530

    jsx img anchor tag

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import ReactDOM from 'react-dom';
 var t="Ritik";
 const currDate = new Date().toLocaleDateString();
 const currTime = new Date().toLocaleTimeString();
+const img1= "https://picsum.photos/200/300";
+const img2= "https://picsum.photos/250/300";
+const img3= "https://picsum.photos/300/300";
+const links= "https://www.facebook.com";
+
 ReactDOM.render(
 <>
 <h1>{`Hello, My Name is ${t}`}</h1>
 <p>{`today Date is = ${currDate}`}</p>
 <p>{`today time is = ${currTime}`}</p>
-
+<h2 contentEditable="true">My name is {t} Raj</h2>\
+<img src={img1} alt="randomimg"/>
+<img src={img2} alt="randomimg"/>
+<a href={links} target="_ritik">
+  <img src={img3} alt="randomimg"/>
+</a>
 </>,
   document.getElementById("root"));
